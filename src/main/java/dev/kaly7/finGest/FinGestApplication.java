3c3c9db68b1d52695ea9380c3691566ec9c0572a
Borelli <Borelli.Fotso@adorsys.com>
2024-07-18 06:15:08 +0100

    initial commit

---
+++ src/main/java/dev/kaly7/finGest/FinGestApplication.java
@@ -0,0 +1,13 @@
+package dev.kaly7.finGest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinGestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinGestApplication.class, args);
+	}
+
+}
