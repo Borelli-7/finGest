38bbc811e38ee636008ac192b2ac60b6c90c7457
Borelli-7 <kaly.salas.7@gmail.com>
2025-01-17 08:02:38 +0100

    update config files

--- src/main/java/dev/kaly7/finGest/FinGestApplication.java
+++ src/main/java/dev/kaly7/finGest/FinGestApplication.java
@@ -2,8 +2,10 @@ package dev.kaly7.finGest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "dev.kaly7.finGest.entities")
 public class FinGestApplication {
 
 	public static void main(String[] args) {
